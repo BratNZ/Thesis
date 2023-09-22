b212cef8d1e893fd1f410e8bfe1552a242072af8
BratNZ <brett@davidson.gen.nz>
2023-09-22 20:42:39 +1200

    Updated imudata.py to apply calibration factors

--- imudata.py
+++ imudata.py
@@ -34,15 +34,15 @@ for channel in enabled_channel_numberarray:
     # 4095
 
 # Correction values from calibration
-#gyro_x_offset = [-0.02998955, -0.03219639,0.023254980,0.008131502,-0.00307724]
-#gyro_y_offset = [0.01596670,0.00790274,0.00284742,-0.00708547,0.00518083]
-#gyro_z_offset = [0.00715132,-0.00007248,-0.00007248,-0.00660733,0.00312161,-0.00114486]
-#accel_x_scale = [0.10203479,0.10182106,0.10181778,0.10188015,0.10167629]
-#accel_y_scale = [-0.10180081,0.10145538,0.10158317,0.10185943,0.10189014]
-#accel_z_scale = [0.10115512,0.1009117,0.10140077,0.10123609,0.10135164]
-#accel_x_offset = [0.00078995,-0.00159294,0.01240663,-0.00665329,0.0027521]
-#accel_y_offset = [-0.01058559,0.01550039,0.0155684,-0.01244544,0.0128008]
-#accel_z_offset = [-0.02925765,-0.01817877,-0.03045218,-0.01997862,0.00237755]
+gyro_x_offset = [-0.02998955, -0.03219639,0.023254980,0.008131502,-0.00307724]
+gyro_y_offset = [0.01596670,0.00790274,0.00284742,-0.00708547,0.00518083]
+gyro_z_offset = [0.00715132,-0.00007248,-0.00007248,-0.00660733,0.00312161,-0.00114486]
+accel_x_scale = [0.10203479,0.10182106,0.10181778,0.10188015,0.10167629]
+accel_y_scale = [-0.10180081,0.10145538,0.10158317,0.10185943,0.10189014]
+accel_z_scale = [0.10115512,0.1009117,0.10140077,0.10123609,0.10135164]
+accel_x_offset = [0.00078995,-0.00159294,0.01240663,-0.00665329,0.0027521]
+accel_y_offset = [-0.01058559,0.01550039,0.0155684,-0.01244544,0.0128008]
+accel_z_offset = [-0.02925765,-0.01817877,-0.03045218,-0.01997862,0.00237755]
  
 timestamp=datetime.now().strftime('%Y-%m-%d_%H%M')
 filename= '' + timestamp + '.csv'
@@ -57,24 +57,24 @@ with open(filename,'a') as file_handle:
       channel_index=enabled_channel_numberarray.index(channel)
       channel_name=enabled_channel_namearray[channel_index]
       elapsedtime=str(time.perf_counter() - initialtime)
-      #acceleration=list(icm.acceleration)
-      acceleration=','.join(map(str,icm.acceleration))
+      acceleration=list(icm.acceleration)
+      #acceleration=','.join(map(str,icm.acceleration))
       #print("Raw Accel =",acceleration,"for IMU",channel)
-      #acceleration[0] = acceleration[0]*accel_x_scale[channel_index]
-      #acceleration[0] = acceleration[0]+accel_x_offset[channel_index]
-      #acceleration[1] = acceleration[1]*accel_y_scale[channel_index]
-      #acceleration[1] = acceleration[1]+accel_y_offset[channel_index]
-      #acceleration[2] = acceleration[2]*accel_z_scale[channel_index]
-      #acceleration[2] = acceleration[2]+accel_z_offset[channel_index]
-      #accelerationstr=','.join(map(str,acceleration))
+      acceleration[0] = acceleration[0]*accel_x_scale[channel_index]
+      acceleration[0] = acceleration[0]+accel_x_offset[channel_index]
+      acceleration[1] = acceleration[1]*accel_y_scale[channel_index]
+      acceleration[1] = acceleration[1]+accel_y_offset[channel_index]
+      acceleration[2] = acceleration[2]*accel_z_scale[channel_index]
+      acceleration[2] = acceleration[2]+accel_z_offset[channel_index]
+      accelerationstr=','.join(map(str,acceleration))
       #print("Calibrated Accel = ",accelerationstr)
-      #gyro=list(icm.gyro)
-      gyro=','.join(map(str,icm.gyro))
+      gyro=list(icm.gyro)
+      #gyro=','.join(map(str,icm.gyro))
       #print(" Raw Gyro = ",gyro)
-      #gyro[0] = gyro[0]+gyro_x_offset[channel_index]
-      #gyro[1] = gyro[1]+gyro_y_offset[channel_index]
-      #gyro[2] = gyro[2]+gyro_z_offset[channel_index]
-      #gyrostr=','.join(map(str,gyro))
+      gyro[0] = gyro[0]+gyro_x_offset[channel_index]
+      gyro[1] = gyro[1]+gyro_y_offset[channel_index]
+      gyro[2] = gyro[2]+gyro_z_offset[channel_index]
+      gyrostr=','.join(map(str,gyro))
       #print(" Calibrated Gyro = ",gyrostr)
       op_line=','.join([channel_name,elapsedtime,acceleration,gyro]) 
       op_line+="\n"
